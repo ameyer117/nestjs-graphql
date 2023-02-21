03e047cd17772b0bee04b02fbb6c866648b4d2f1
Alec Meyer <abmeyer@stackarmor.com>
2023-02-21 10:54:52 -0500

    initial commit

---
+++ src/users/users.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserInput } from './dto/input/create-user.input';
+import { DeleteUserInput } from './dto/input/delete-user.input';
+import { UpdateUserInput } from './dto/input/update-user.input';
+import { User } from './models/user';
+import { v4 as uuidv4 } from 'uuid';
+import { GetUserArgs } from './dto/args/get-user.args';
+import { GetUsersArgs } from './dto/args/get-users.args';
+
+@Injectable()
+export class UsersService {
+  private users: User[] = [];
+
+  public getUsers(getUsersArgs: GetUsersArgs) {
+    return getUsersArgs.userIds.map((userId) => this.getUserById({ userId }));
+  }
+
+  public getUserById(getUserArgs: GetUserArgs) {
+    return this.users.find((user) => user.userId === getUserArgs.userId);
+  }
+
+  public createUser(createUserData: CreateUserInput) {
+    const user: User = {
+      userId: uuidv4(),
+      ...createUserData,
+    };
+
+    this.users.push(user);
+  }
+
+  public updateUser(updateUserData: UpdateUserInput) {
+    const userIndex = this.users.findIndex(
+      (user) => user.userId === updateUserData.userId,
+    );
+    if (userIndex === -1) return;
+    this.users[userIndex] = { ...this.users[userIndex], ...updateUserData };
+  }
+
+  public deleteUser(deleteUserData: DeleteUserInput) {
+    const userIndex = this.users.findIndex(
+      (user) => user.userId === deleteUserData.userId,
+    );
+    if (userIndex === -1) return;
+    this.users.splice(userIndex, 1);
+  }
+}
